689b6ccfc54b390f7805c0b3774b12a48850a97c
acaberoy <a.caberoy.551520@umindanao.edu.ph>
2025-09-13 19:28:43 +0800

    Add files via upload

---
+++ register.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h1>Register</h1>
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form method="POST" action="{{ route('register.submit') }}">
+        @csrf
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" value="{{ old('name') }}" required><br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" required><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br>
+        <label for="password_confirmation">Confirm Password:</label>
+        <input type="password" id="password_confirmation" name="password_confirmation" required><br>
+        <button type="submit">Register</button>
+    </form>
+</body>
+</html>
---
+++ success.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Success</title>
+</head>
+<body>
+    <h1>Registration Successful!</h1>
+    <p>Your account has been created.</p>
+    <p>Name: {{ $user->name }}</p>
+    <p>Email: {{ $user->email }}</p>
+</body>
+</html>
