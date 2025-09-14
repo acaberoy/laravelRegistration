c03380dec7b9535175db4934a5340c38406f2b2c
acaberoy <a.caberoy.551520@umindanao.edu.ph>
2025-09-14 16:02:58 +0800

    Add files via upload

---
+++ login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    @if ($errors->any())
+        <div style="color: red;">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form method="POST" action="{{ route('login.submit') }}">
+        @csrf
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" required><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
